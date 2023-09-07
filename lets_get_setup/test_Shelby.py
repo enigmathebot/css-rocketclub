83ffa7d58f0246fd6633deb7c290298e11c54404
Shelby Palmer <shelbypalmer@Shelbys-MacBook-Pro.local>
2023-09-06 20:17:24 -0400

    first commit

---
+++ lets_get_setup/test_Shelby.py
@@ -0,0 +1,14 @@
+#calculator!!
+
+#program structure - input (string of numbers/symbols)
+#splice and convert
+#insert into algorithm
+while True:
+    try:
+        calc_input=int(input("<Enter numbers>"))
+        break
+    except ValueError:
+        print("That was not a valid input. Try again...")
+    
+
+
